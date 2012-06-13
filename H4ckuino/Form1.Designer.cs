07d61ad5fb57ed120a11c2d549b6571cbcceb5fb
gpeal <gabriel@gpeal.com>
2012-06-12 21:50:15 -0500

    Initial Commit

---
+++ H4ckuino/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace H4ckuino
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comComboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "COM PORT";
+            // 
+            // comComboBox
+            // 
+            this.comComboBox.FormattingEnabled = true;
+            this.comComboBox.Location = new System.Drawing.Point(100, 25);
+            this.comComboBox.Name = "comComboBox";
+            this.comComboBox.Size = new System.Drawing.Size(121, 24);
+            this.comComboBox.TabIndex = 1;
+            this.comComboBox.SelectedIndexChanged += new System.EventHandler(this.comComboBox_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(748, 375);
+            this.Controls.Add(this.comComboBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Arduino PISystem Connector";
+            this.Deactivate += new System.EventHandler(this.Form1_Deactivate);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comComboBox;
+    }
+}
+
---
+++ H4ckuino/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using PISDK;
+using System.Threading;
+using System.IO.Ports;
+
+namespace H4ckuino
+{
+    public partial class Form1 : Form
+    {
+
+        SerialPort serialPort;
+        PIPoint piPoint;
+        PISDK.PISDK piSDK;
+        PISDK.Server piServer;
+        string tagName = "HackathonRampData";
+        string serverName = "piserver1";
+
+        public Form1()
+        {
+            InitializeComponent();
+            string[] ports = SerialPort.GetPortNames();
+            serialPort = new SerialPort();
+            serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+            foreach (string port in ports)
+            {
+                comComboBox.Items.Add(port);
+            }
+            piSDK = new PISDK.PISDKClass();
+            piServer = piSDK.Servers[serverName];
+            piServer.Open("");
+            if (piServer.Connected)
+                Console.WriteLine("Connected to Server");
+            piPoint = readPIPoint(piServer, tagName);
+        }
+
+        private void writePI(PIPoint piPoint, int index)
+        {
+            piPoint.Data.UpdateValue(index, 0, DataMergeConstants.dmReplaceDuplicates);
+        }
+
+        private PIPoint readPIPoint(PISDK.Server piServer, string tagName)
+        {
+            return piSDK.GetPoint("\\\\" + piServer.Path.ToString() + "\\" + tagName);
+        }
+
+        private void comComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Allow the user to set the appropriate properties.
+            serialPort.PortName = comComboBox.SelectedItem.ToString();
+            serialPort.BaudRate = 9600;
+            serialPort.Open();
+            if(serialPort.IsOpen)
+                Console.WriteLine(serialPort.PortName + " is open");
+        }
+
+        private void Form1_Deactivate(object sender, EventArgs e)
+        {
+            serialPort.Close();
+        }
+
+        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            string data = serialPort.ReadExisting();
+            int intData;
+            if(Int32.TryParse(data, out intData)) {
+                Console.Write("Data Received:");
+                Console.WriteLine(data);
+                writePI(piPoint, intData);
+            }
+        }
+    }
+}
